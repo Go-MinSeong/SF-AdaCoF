635ee7d2c7f3d674b912693d1fcf711797518f52
KoMinSeong <91547241+Go-MinSeong@users.noreply.github.com>
2023-12-07 13:47:00 +0900

    Add files via upload

--- interpolate.sh
+++ interpolate.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 #  pip install cupy-cuda111
-#  pip install pytube
+pip install pytube
 
 # make dataset
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=aTh-FjI5__Q" --name "LE SSERAFIM (르세라핌) 'ANTIFRAGILE' Dance Cover Mirrored (거울모드)_30.0" --start "90" --end "3600" --interval 1  #제한한
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=qJTAGJBVmys" --name "(여자)아이들(G)-IDLE 'TOMBOY (톰보이)' DANCECOVER | 안무 거울모드 | MIRROR_30.0" --start "60" --end "5160" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=jH8OIsiIHoI" --name "[ (여자)아이들((G)I-DLE) - 'Nxde(누드)' 안무 거울모드 MIRRORED | 커버댄스 DANCECOVER | 1인안무 ]_30.0" --start "40" --end "5300" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=EMQaCq-S-LE" --name "[MIRRORED] Brave Girls(브레이브 걸스) - Rollin’ 안무 거울모드_24.0" --start "50" --end "4000" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=Z9Sn9r82gyE" --name "[MIRRORED] Hwa Sa(화사) - Maria(마리아) Dance Cover 커버댄스 거울모드 안무_30.0" --start "250" --end "5900" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=7Eg2vSjAAFk" --name "뉴진스 NewJeans 'Attention(어텐션)' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "40" --end "3050" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=eIBVFbrvpng" --name "뉴진스 NewJeans 'Hype boy' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "120" --end "2600" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=LlBG2ipO6ZU" --name "뉴진스 NewJeans ‘Ditto' DANCECOVER | MIRRORED | 안무 거울모드_30.0" --start "500" --end "5400" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=wWA3ICLkSD4&t=182s" --name "다시 만난 세계 안무 커버 [거울모드]_30.0" --start "250" --end "5000" --interval 1
+python MakeTripletset.py --video_url "https://www.youtube.com/watch?v=_Y7wlTk1-XQ" --name "H.O.T - 캔디(Candy) 안무 거울모드_29.97002997002997" --start "90" --end "3300" --interval 1
 
-# python MakeTripletset.py --name "(여자)아이들(G)-IDLE 'TOMBOY (톰보이)' DANCECOVER | 안무 거울모드 | MIRROR_30.0" --start "60" --end "5160" --interval 1
-# python MakeTripletset.py --name "[ (여자)아이들((G)I-DLE) - 'Nxde(누드)' 안무 거울모드 MIRRORED | 커버댄스 DANCECOVER | 1인안무 ]_30.0" --start "40" --end "5300" --interval 1
-# python MakeTripletset.py --name "[MIRRORED] Brave Girls(브레이브 걸스) - Rollin’ 안무 거울모드_24.0" --start "50" --end "4000" --interval 1
-# python MakeTripletset.py --name "[MIRRORED] Hwa Sa(화사) - Maria(마리아) Dance Cover 커버댄스 거울모드 안무_30.0" --start "250" --end "5900" --interval 1
-# python MakeTripletset.py --name "뉴진스 NewJeans 'Attention(어텐션)' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "40" --end "3050" --interval 1
-# python MakeTripletset.py --name "뉴진스 NewJeans 'Hype boy' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "120" --end "2600" --interval 1
-# python MakeTripletset.py --name "뉴진스 NewJeans ‘Ditto' DANCECOVER | MIRRORED | 안무 거울모드_30.0" --start "500" --end "5400" --interval 1
-# python MakeTripletset.py --name "다시 만난 세계 안무 커버 [거울모드]_30.0" --start "250" --end "5000" --interval 1
-# python MakeTripletset.py --name "LE SSERAFIM (르세라핌) 'ANTIFRAGILE' Dance Cover Mirrored (거울모드)_30.0" --start "90" --end "3600" --interval 1
-# python MakeTripletset.py --name "H.O.T - 캔디(Candy) 안무 거울모드_29.97002997002997" --start "90" --end "3300" --interval 1
+python MakeTripletset.py --name "(여자)아이들(G)-IDLE 'TOMBOY (톰보이)' DANCECOVER | 안무 거울모드 | MIRROR_30.0" --start "60" --end "5160" --interval 2
+python MakeTripletset.py --name "[ (여자)아이들((G)I-DLE) - 'Nxde(누드)' 안무 거울모드 MIRRORED | 커버댄스 DANCECOVER | 1인안무 ]_30.0" --start "40" --end "5300" --interval 2
+python MakeTripletset.py --name "[MIRRORED] Brave Girls(브레이브 걸스) - Rollin’ 안무 거울모드_24.0" --start "50" --end "4000" --interval 2
+python MakeTripletset.py --name "[MIRRORED] Hwa Sa(화사) - Maria(마리아) Dance Cover 커버댄스 거울모드 안무_30.0" --start "250" --end "5900" --interval 2
+python MakeTripletset.py --name "뉴진스 NewJeans 'Attention(어텐션)' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "40" --end "3050" --interval 2
+python MakeTripletset.py --name "뉴진스 NewJeans 'Hype boy' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "120" --end "2600" --interval 2
+python MakeTripletset.py --name "뉴진스 NewJeans ‘Ditto' DANCECOVER | MIRRORED | 안무 거울모드_30.0" --start "500" --end "5400" --interval 2
+python MakeTripletset.py --name "다시 만난 세계 안무 커버 [거울모드]_30.0" --start "250" --end "5000" --interval 2
+python MakeTripletset.py --name "LE SSERAFIM (르세라핌) 'ANTIFRAGILE' Dance Cover Mirrored (거울모드)_30.0" --start "90" --end "3600" --interval 2
+python MakeTripletset.py --name "H.O.T - 캔디(Candy) 안무 거울모드_29.97002997002997" --start "90" --end "3300" --interval 2
 
-# python MakeTripletset.py --name "(여자)아이들(G)-IDLE 'TOMBOY (톰보이)' DANCECOVER | 안무 거울모드 | MIRROR_30.0" --start "60" --end "5160" --interval 2
-# python MakeTripletset.py --name "[ (여자)아이들((G)I-DLE) - 'Nxde(누드)' 안무 거울모드 MIRRORED | 커버댄스 DANCECOVER | 1인안무 ]_30.0" --start "40" --end "5300" --interval 2
-# python MakeTripletset.py --name "[MIRRORED] Brave Girls(브레이브 걸스) - Rollin’ 안무 거울모드_24.0" --start "50" --end "4000" --interval 2
-# python MakeTripletset.py --name "[MIRRORED] Hwa Sa(화사) - Maria(마리아) Dance Cover 커버댄스 거울모드 안무_30.0" --start "250" --end "5900" --interval 2
-# python MakeTripletset.py --name "뉴진스 NewJeans 'Attention(어텐션)' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "40" --end "3050" --interval 2
-# python MakeTripletset.py --name "뉴진스 NewJeans 'Hype boy' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "120" --end "2600" --interval 2
-# python MakeTripletset.py --name "뉴진스 NewJeans ‘Ditto' DANCECOVER | MIRRORED | 안무 거울모드_30.0" --start "500" --end "5400" --interval 2
-# python MakeTripletset.py --name "다시 만난 세계 안무 커버 [거울모드]_30.0" --start "250" --end "5000" --interval 2
-# python MakeTripletset.py --name "LE SSERAFIM (르세라핌) 'ANTIFRAGILE' Dance Cover Mirrored (거울모드)_30.0" --start "90" --end "3600" --interval 2
-# python MakeTripletset.py --name "H.O.T - 캔디(Candy) 안무 거울모드_29.97002997002997" --start "90" --end "3300" --interval 2
-
-# python MakeTripletset.py --name "(여자)아이들(G)-IDLE 'TOMBOY (톰보이)' DANCECOVER | 안무 거울모드 | MIRROR_30.0" --start "60" --end "5160" --interval 3
-# python MakeTripletset.py --name "[ (여자)아이들((G)I-DLE) - 'Nxde(누드)' 안무 거울모드 MIRRORED | 커버댄스 DANCECOVER | 1인안무 ]_30.0" --start "40" --end "5300" --interval 3
-# python MakeTripletset.py --name "[MIRRORED] Brave Girls(브레이브 걸스) - Rollin’ 안무 거울모드_24.0" --start "50" --end "4000" --interval 3
-# python MakeTripletset.py --name "[MIRRORED] Hwa Sa(화사) - Maria(마리아) Dance Cover 커버댄스 거울모드 안무_30.0" --start "250" --end "5900" --interval 3
-# python MakeTripletset.py --name "뉴진스 NewJeans 'Attention(어텐션)' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "40" --end "3050" --interval 3
-# python MakeTripletset.py --name "뉴진스 NewJeans 'Hype boy' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "120" --end "2600" --interval 3
-# python MakeTripletset.py --name "뉴진스 NewJeans ‘Ditto' DANCECOVER | MIRRORED | 안무 거울모드_30.0" --start "500" --end "5400" --interval 3
-# python MakeTripletset.py --name "다시 만난 세계 안무 커버 [거울모드]_30.0" --start "250" --end "5000" --interval 3
-# python MakeTripletset.py --name "LE SSERAFIM (르세라핌) 'ANTIFRAGILE' Dance Cover Mirrored (거울모드)_30.0" --start "90" --end "3600" --interval 3
-# python MakeTripletset.py --name "H.O.T - 캔디(Candy) 안무 거울모드_29.97002997002997" --start "90" --end "3300" --interval 3
+python MakeTripletset.py --name "(여자)아이들(G)-IDLE 'TOMBOY (톰보이)' DANCECOVER | 안무 거울모드 | MIRROR_30.0" --start "60" --end "5160" --interval 3
+python MakeTripletset.py --name "[ (여자)아이들((G)I-DLE) - 'Nxde(누드)' 안무 거울모드 MIRRORED | 커버댄스 DANCECOVER | 1인안무 ]_30.0" --start "40" --end "5300" --interval 3
+python MakeTripletset.py --name "[MIRRORED] Brave Girls(브레이브 걸스) - Rollin’ 안무 거울모드_24.0" --start "50" --end "4000" --interval 3
+python MakeTripletset.py --name "[MIRRORED] Hwa Sa(화사) - Maria(마리아) Dance Cover 커버댄스 거울모드 안무_30.0" --start "250" --end "5900" --interval 3
+python MakeTripletset.py --name "뉴진스 NewJeans 'Attention(어텐션)' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "40" --end "3050" --interval 3
+python MakeTripletset.py --name "뉴진스 NewJeans 'Hype boy' DANCECOVER | 안무 거울모드 | MIRRORED_30.0" --start "120" --end "2600" --interval 3
+python MakeTripletset.py --name "뉴진스 NewJeans ‘Ditto' DANCECOVER | MIRRORED | 안무 거울모드_30.0" --start "500" --end "5400" --interval 3
+python MakeTripletset.py --name "다시 만난 세계 안무 커버 [거울모드]_30.0" --start "250" --end "5000" --interval 3
+python MakeTripletset.py --name "LE SSERAFIM (르세라핌) 'ANTIFRAGILE' Dance Cover Mirrored (거울모드)_30.0" --start "90" --end "3600" --interval 3
+python MakeTripletset.py --name "H.O.T - 캔디(Candy) 안무 거울모드_29.97002997002997" --start "90" --end "3300" --interval 3
 
 
 
@@ -138,3 +137,8 @@
 # python /home/work/capstone/Final/interpolate_video.py --video_url "https://www.youtube.com/watch?v=EMQaCq-S-LE"
 # python /home/work/capstone/Final/interpolate_video.py --video_url "https://www.youtube.com/watch?v=EMQaCq-S-LE" --frame 10
 
+# python /home/work/capstone/SF-AdaCoF/evaluation.py --model_dir "AdaCoF_1"
+# python /home/work/capstone/SF-AdaCoF/evaluation.py --model_dir "AdaCoF_2"
+# python /home/work/capstone/SF-AdaCoF/evaluation.py --model_dir "AdaCoF_3"
+# python /home/work/capstone/SF-AdaCoF/evaluation.py --model_dir "AdaCoF_all_xshare"
+# python /home/work/capstone/SF-AdaCoF/evaluation.py --model_dir "SF_AdaCoF"
